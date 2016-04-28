6b1997524f58b871e8ebe2a2b76ad387e92e267d
Josh Hodgson <josh.pbh@gmail.com>
2016-04-28 13:24:02 +0100

    Ben did this, He can have the credit
    Added the gridCtril to js for CG.js page

--- public/js/cg.js
+++ public/js/cg.js
@@ -37,13 +37,13 @@ app.controller('boxingCtrl', ['$scope', 'socket',
         socket.on("clock:tick", function (msg) {
             $scope.clock = msg.slice(0, msg.indexOf("."));
         });
-        
+
         $scope.$watch('boxing', function() {
             if (!$scope.boxing) {
                 getBoxingData();
             }
         }, true);
-        
+
         function getBoxingData() {
             socket.emit("boxing:get");
             socket.emit("clock:get");
@@ -58,13 +58,13 @@ app.controller('bugCtrl', ['$scope', '$timeout', 'socket',
         socket.on("bug", function (state) {
             $scope.state = state;
         });
-        
+
         $scope.$watch('bug', function() {
             if (!$scope.state) {
                 getBugData();
             }
         }, true);
-        
+
         function getBugData() {
             socket.emit("bug:get");
         };
@@ -98,13 +98,13 @@ app.controller('scoringCtrl', ['$scope', '$timeout', '$http', 'socket',
             console.log(state);
             $scope.showScore = state.showScore;
         });
-        
+
         $scope.$watch('score', function() {
             if (!$scope.score) {
                 getScoreData();
             }
         }, true);
-        
+
         function getScoreData() {
             socket.emit("score:get");
         };
@@ -113,7 +113,7 @@ app.controller('scoringCtrl', ['$scope', '$timeout', '$http', 'socket',
         fetchScore();
         // Start the timer
         $timeout(fetchScore, $scope.tickInterval);
-    }   
+    }
 ]);
 
 app.controller('footballCtrl', ['$scope', 'socket',
@@ -126,13 +126,13 @@ app.controller('footballCtrl', ['$scope', 'socket',
         socket.on("clock:tick", function (msg) {
             $scope.clock = msg.slice(0, msg.indexOf("."));
         });
-        
+
         $scope.$watch('football', function() {
             if (!$scope.football) {
                 getFootballData();
             }
         }, true);
-        
+
         function getFootballData() {
             socket.emit("football:get");
             socket.emit("clock:get");
@@ -145,41 +145,56 @@ app.controller('dartsCtrl', ['$scope', 'socket',
         socket.on("dart", function (msg) {
             $scope.darts = msg;
         });
-        
+
         $scope.$watch('dart', function() {
             if (!$scope.dart) {
                 getDartData();
             }
         }, true);
-        
+
         function getDartData() {
             socket.emit("dart:get");
         };
     }
 ]);
 
+app.controller('gridCtrl', ['$scope', 'socket',
+    function($scope, socket){
+        socket.on("grid", function (payload) {
+            if (payload === "hide") {
+                //We first remove every element with a delay
+                $scope.grid = {};
+                $scope.show = false;
+            } else {
+                $scope.show = true;
+                $scope.grid = payload;
+            }
+        });
+    }
+])
+
 app.controller('swimmingCtrl', ['$scope', 'socket',
     function($scope, socket){
         socket.on("swimming", function (msg) {
             $scope.swimming = msg;
         });
-        
+
         $scope.clockMin = "0";
         $scope.clockSec = "00";
         $scope.clockDec = "0";
-        
+
         socket.on("clock:tick", function (msg) {
             $scope.clockMin = msg.slice(0,msg.indexOf(":")).replace(/^0/, '');
             $scope.clockSec = msg.slice(msg.indexOf(":")+1,msg.indexOf("."));
             $scope.clockDec = msg.slice(msg.indexOf(".")+1);
         });
-        
+
         $scope.$watch('swimming', function() {
             if (!$scope.swimming) {
                 getSwimmingData();
             }
         }, true);
-        
+
         function getSwimmingData() {
             socket.emit("swimming:get");
             socket.emit("clock:get");
